0a12a94c5e7fa1937c9764f7c73b3952d25e2896
hailey <dohye0806@gmail.com>
2024-10-02 13:06:59 +0900

    [ADD] #1~5

--- src/main/java/com/sds/task/repository/TaskRepository.java
+++ src/main/java/com/sds/task/repository/TaskRepository.java
@@ -3,5 +3,8 @@ package com.sds.task.repository;
 import com.sds.task.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task> findTaskById(Long id);
 }
